b64c1bf42bc720ae69fe6dac87c0fc409c905a3d
Bakkiyaraj <sanjay0530@yahoo.co.in>
2019-08-27 19:03:14 +0530

    initial commit for user management serer side code base

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    db: {
+        // url: 'ds263109.mlab.com:63109',
+        url: 'devconnectors-mvedo.mongodb.net',
+        database: 'devconnector',
+        user: 'abc',
+        password: 'abc'
+    }
+}
+
+// mongodb+srv://abc:abc@devconnectors-mvedo.mongodb.net/devconnector
